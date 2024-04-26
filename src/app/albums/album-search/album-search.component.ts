70bd82ff6d3c7990e41340bea726635c90eefd5f
Nathanael Law <njlaw@xyrodian.com>
2024-04-26 16:05:14 +0000

    m1 done

--- src/app/albums/album-search/album-search.component.ts
+++ src/app/albums/album-search/album-search.component.ts
@@ -1,58 +1,87 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
 import { ProgressBarComponent } from '@/shared/ui/progress-bar.component';
 import { SortOrder } from '@/shared/models/sort-order.model';
-import { Album } from '@/albums/album.model';
+import { Album, searchAlbums, sortAlbums } from '@/albums/album.model';
 import { AlbumFilterComponent } from './album-filter/album-filter.component';
 import { AlbumListComponent } from './album-list/album-list.component';
+import { patchState, signalState } from '@ngrx/signals';
+import { AlbumsService } from '@/albums/albums.service';
+import { catchError, EMPTY } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+interface AlbumsState {
+  albums: Album[],
+  showProgress: boolean;
+  query: string;
+  order: SortOrder;
+}
+const albumsState = signalState<AlbumsState>({
+  albums: [],
+  showProgress: false,
+  query: '',
+  order: 'asc',
+});
 
 @Component({
   selector: 'ngrx-album-search',
   standalone: true,
   imports: [ProgressBarComponent, AlbumFilterComponent, AlbumListComponent],
   template: `
-    <ngrx-progress-bar [showProgress]="showProgress" />
+    <ngrx-progress-bar [showProgress]="showProgress()" />
 
     <div class="container">
-      <h1>Albums ({{ totalAlbums }})</h1>
+      <h1>Albums ({{ totalAlbums() }})</h1>
 
       <ngrx-album-filter
-        [query]="query"
-        [order]="order"
+        [query]="query()"
+        [order]="order()"
         (queryChange)="updateQuery($event)"
         (orderChange)="updateOrder($event)"
       />
 
-      <ngrx-album-list [albums]="albums" [showSpinner]="showSpinner" />
+      <ngrx-album-list [albums]="filteredAlbums()" [showSpinner]="showSpinner()" />
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AlbumSearchComponent {
-  readonly albums: Album[] = [
-    {
-      id: 1,
-      title: 'Album 1',
-      artist: 'Artist 1',
-      releaseDate: '2023-01-01',
-      genre: 'Genre 1',
-      coverImage: '/assets/album-covers/unplugged.jpg',
-    },
-    {
-      id: 2,
-      title: 'Album 2',
-      artist: 'Artist 2',
-      releaseDate: '2024-01-01',
-      genre: 'Genre 2',
-      coverImage: '/assets/album-covers/are-you-experienced.jpg',
-    },
-  ];
-  readonly query = '';
-  readonly order: SortOrder = 'asc';
-  readonly showSpinner = false;
-  readonly showProgress = false;
-  readonly totalAlbums = this.albums.length;
+  readonly albums = albumsState.albums;
+  readonly query = albumsState.query;
+  readonly order = albumsState.order;
+  readonly filteredAlbums = computed(() => {
+    return sortAlbums(searchAlbums(albumsState.albums(), albumsState.query()), albumsState.order());
+  });
+  readonly showProgress = albumsState.showProgress;
+  readonly totalAlbums = computed(() => this.filteredAlbums().length);
+  readonly showSpinner = computed(() => {
+    return this.showProgress() && albumsState.albums().length === 0;
+  })
 
-  updateQuery(query: string): void {}
+  constructor(
+    private readonly albumsService: AlbumsService,
+    private readonly snackBar: MatSnackBar,
+  ) {
+    patchState(albumsState, { showProgress: true });
+    this.albumsService.getAll().pipe(
+      catchError(() => {
+        snackBar.open('Error fetching albums', 'Error', { duration: 3000 });
+        patchState(albumsState, { showProgress: false });
+        return EMPTY;
+      }),
+    ).subscribe((albums: Album[]) => {
+        patchState(
+          albumsState,
+          { albums },
+          { showProgress: false },
+        )
+      });
+  }
 
-  updateOrder(order: SortOrder): void {}
+  updateQuery(query: string): void {
+    patchState(albumsState, { query });
+  }
+
+  updateOrder(order: SortOrder): void {
+    patchState(albumsState, { order });
+  }
 }
